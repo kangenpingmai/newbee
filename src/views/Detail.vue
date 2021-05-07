22ad6e89a2fcfc1a4d8762b9173d1e26eb366979
wangpeng <wang455773950@163.com>
2021-05-07 20:42:36 +0800

    add public header navigation

--- src/views/Detail.vue
+++ src/views/Detail.vue
@@ -1,14 +1,18 @@
 <template>
-  <div class="home">
-    <div>我是detail</div>
+  <div>
+    <s-header :title="'详情'"></s-header>
   </div>
 </template>
 
 <script>
+import sHeader from '@/components/SimpleHeader'
 export default {
-  name: 'Detail'
+  components: {
+    sHeader
+  }
 }
 </script>
 
-<style lang="less" scoped>
+<style>
+
 </style>
